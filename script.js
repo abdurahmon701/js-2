a582e25093589ebbcf09e1e1e39c3519f91136a7
abdurahmon70101~ <abdurahmonabdumavlonov750@gmail.com>
2024-08-16 23:54:02 +0500

    mjmcirjmer

---
+++ script.js
@@ -0,0 +1,11 @@
+var a = prompt('ismingizni kiriting')
+
+var b = +prompt('parolingizni kiriting')
+
+var c = confirm('tastiqlashingizmi')
+
+console.log(typeof a);
+
+console.log(typeof  b);
+
+console.log(typeof  c);
